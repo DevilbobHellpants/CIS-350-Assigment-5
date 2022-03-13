9f96e792d4e97b20327608d0c1cfc52e95ba11b8
DevilbobHellpants <trillizio3@hotmail.com>
2022-03-13 13:26:22 -0500

    Assignment 5B Complete

---
+++ 3D-Prototype/Assets/MyFirstPersonPlayer/Scripts/DisplayText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public Text displayVictory;
+
+    public bool called = false;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 360)
+        {
+            timer++;
+        }
+        else if (timer >= 360)
+        {
+            if (called == false)
+            {
+                displayVictory.text = "";
+                called = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("MyFirstPersonPlayer");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("display");
+            Destroy(gameObject);
+            displayVictory.text = "Congratulations, you did it! You Win! Press R to restart.";
+        }
+    }
+}
---
+++ 3D-Prototype/Assets/MyFirstPersonPlayer/Scripts/LightingManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    public Light mainLight;
+    public float flashing;
+
+    public AudioSource LightningStrikeSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainLight = GameObject.Find("LightMainDirectional").GetComponent<Light>();
+        StartCoroutine(LightningFlashes());
+        flashing = .5f;
+        mainLight.intensity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LightningFlashes()
+    {
+        //float waitTime = Random.Range(1f, 3f);
+        do
+        {
+            yield return new WaitForSeconds(Random.Range(8, 10));
+
+            mainLight.intensity = .2f;
+            LightningStrikeSound.Play();
+
+            yield return new WaitForSeconds(flashing);
+
+            mainLight.intensity = 0f;
+
+            yield return new WaitForSeconds(flashing);
+
+            mainLight.intensity = .2f;
+
+            yield return new WaitForSeconds(flashing);
+
+            mainLight.intensity = 0f;
+
+            yield return new WaitForSeconds(flashing);
+
+            mainLight.intensity = .2f;
+
+            yield return new WaitForSeconds(flashing);
+
+            mainLight.intensity = 0f;
+        } while (true);
+    }
+}
